d6411da9c0778e6b56dbe6aea3ed6d95c7cf60dc
Victor Botina Jojoa <victorabj@gmail.com>
2025-12-17 04:46:35 +0000

    Solución 4: Usando la documentación oficial de Supabase Según la documen

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,3 +1,4 @@
+
 import { createSupabaseServerClient } from '@/lib/supabase/server'
 import { NextResponse } from 'next/server'
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   description: 'A clean and stable Next.js starter project.',
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
