f7c834a63136fd1c8b3e840d183850dcaf4012eb
Victor Botina Jojoa <victorabj@gmail.com>
2026-01-08 03:54:54 +0000

    esta solucion  es persistente y solucionar esta conflicto de información

--- src/lib/actions/ad-data.actions.ts
+++ src/lib/actions/ad-data.actions.ts
@@ -3,9 +3,6 @@
 
 import { createSupabaseServerClient } from "@/lib/supabase/server";
 import type { AdForTable } from "../types";
-import { format } from 'date-fns';
-import { es } from 'date-fns/locale';
-import { parseISO } from 'date-fns';
 
 export async function getAdsForAdvertiser(query: string, status: string) {
   const supabase = await createSupabaseServerClient();
